7ad8fb9f8d73e358c61cb194220f5d491805ff2e
maxwell <1507499759@qq.com>
2020-09-07 22:11:25 +0800

    用户头像上传

--- routes/admin/newsRouter.js
+++ routes/admin/newsRouter.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* 后台新闻编辑页管理. */
-router.get('/', function(req, res, next) {
-  res.send('后台新闻编辑页管理')
+router.get('/', function(req, res) {
+  res.render('img')
+  // res.send('后台新闻编辑页管理')
 });
 
 module.exports = router;
--- routes/admin/userRouter.js
+++ routes/admin/userRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const fs = require('fs')
+const path = require('path')
+const multer = require('multer')
+const os = require('os')
 
 const router = express.Router();
 const sqlQuery = require('../../public/module/lcMysql')
@@ -17,11 +20,18 @@ router.get('/info',async (req,res) => {
   let sqlStr = "select * from user where username = ?"
   let result = await sqlQuery(sqlStr,[username])
   result = result[0]
+  let imgSrc = ''
+  if(!result.imgHead){
+    imgSrc = 'users/default.jpg'
+  }else{
+    imgSrc = result.imgHead
+  }
   res.render('admin/info.ejs',{
     username: username,
     mobile: result.mobile,
     email: result.email,
-    password: result.password
+    password: result.password,
+    imgSrc: imgSrc
   })
 })
 router.post('/info',async (req,res) => {
@@ -50,6 +60,50 @@ router.get('/axios',async (req,res) => {
     res.send(false)  //返回false表示不能注册
   }else{
     res.send(true)  //返回true表示可以注册
+  }
+})
+
+// form表单提交头像文件
+router.post('/imghead',multer({
+  dest: './public/users/'
+}).single('file'),async (req,res,next) => {
+  if(!req.file) {
+    res.send('上传文件不能为空！')
+    return
+  }else{
+    // fs.appendFile('')
+    let userFile = './public/users/'+req.session.username
+    let filepath=''
+    fs.mkdir(userFile,(err,data) => {
+      if(err){
+        console.log(err)
+      }else{
+        // console.log(data)
+      }
+    })
+    let file = req.file;
+    let fileInfo = {}
+    // console.log(file)
+    // console.log(file.filename)
+    //写入文件成功
+    fs.renameSync('./public/users/'+file.filename,userFile+'/'+file.originalname)
+    //把文件路径更新到数据库中
+    fileInfo.mimetype = file.mimetype
+    fileInfo.size = file.size
+    fileInfo.path = file.path
+    filepath = 'users/'+req.session.username+'/'+file.originalname
+    let sqlStr = 'update user set imgHead=? where username = ?'
+    await sqlQuery(sqlStr,[filepath,req.session.username])
+
+    //设置响应类型及编码
+    res.set({
+      'content-type': 'application/json; charset=utf-8'
+    })
+    // res.json({
+    //   state:'ok',
+    //   imgUrl: filepath
+    // })
+    res.redirect('http://localhost:3000/admin/user/info')
   }
 })
 
